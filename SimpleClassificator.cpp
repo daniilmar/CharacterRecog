1980780ab6b4919088a5dfa02f0e620770b9f115
daniilmar <daniilmarl@gmail.com>
2015-11-24 09:25:09 +0300

    Update SimpleClassificator.cpp

--- SimpleClassificator.cpp
+++ SimpleClassificator.cpp
@@ -5,28 +5,24 @@
 #include <opencv2/opencv.hpp>
 #include <fstream>
 #include "Preparing.h"
+#include "TrainBase.h"
 
 using namespace cv;
 using namespace cv::ml;
 
-char  character[] = { 'А' ,'Б','В','Е','Н','О','П','Р','Т','У','Х' };
-Mat src;
-Mat src_gray;
-int thresh = 100;
-RNG rng(12345);
-
 int _tmain(int argc, _TCHAR* argv[])
 {
-	KNearest* knn = KNearest::create();
-	
 	string infofile = "char/char/file_info.txt",
 		trainfile = "char/char/train.txt",
 		traininfo = "char/char/traininfo.txt";
 
+	CreateTrainData(infofile, trainfile, traininfo, 18);
+	
 	std::ifstream trinfo(traininfo);
 	if (!trinfo.is_open()) {
 		throw "Can not open " + traininfo;
 	}
+
 	std::ifstream trainData(trainfile);
 	if (!trainData.is_open()) {
 		throw "Can not open " + trainfile;
@@ -48,43 +44,35 @@ int _tmain(int argc, _TCHAR* argv[])
 		for (int j = 0; j < num_features; ++j) {
 			trainData >> trainingDataMat.at<float>(i, j);
 		}
-
 		trainData >> type;
 		labelsMat.at<float>(i, 0) = type;
-
 	}
 	trainData.close();
-
-	//cv:Ptr<TrainData> trainData = TrainData::create(trainingDataMat, 0, labelsMat);
-	knn->setIsClassifier(true);
-	knn->train(trainingDataMat, ml::ROW_SAMPLE, labelsMat);
 	
-	double* features2 = new double[23];
+	double* features = new double[20];
+	int right = 0;
 
-	float* features1 = new float[23];
+	for (int i = 1; i < 56; i++) {
 
-	for (int i = 1; i < 55; i++) {
 		string filename = "char/char/test/" + to_string(i) + ".png";
-		cout << filename << endl;
+
 		Mat sample = imread(filename);
-		getFeatures(sample, features2);
-		for (int i = 0; i < 23; ++i) {
-			features1[i] = features2[i];
-		}
-		Mat query = Mat(1, 23, CV_32F, features1); 
+		getFeatures2(sample, features);
+
+		Mat query = Mat(1, 23, CV_32F, features);
 		Mat res;
 
 		double min = 0;
 		int k = 0;
 		for (int i = 0; i < num_features; i++) {
-			min += (features1[i] - trainingDataMat.at<float>(0, i)) * (features1[i] - trainingDataMat.at<float>(0, i));
+			min += (features[i] - trainingDataMat.at<float>(0, i)) * (features[i] - trainingDataMat.at<float>(0, i));
 		}
 		min = sqrt(min);
 
 		for (int i = 1; i < num_samples - 1; i++) {
 			int tmp = 0;
 			for (int j = 0; j < num_features; j++) {
-				tmp += ((features1[j] - trainingDataMat.at<float>(i, j)) / 1000.0) * ((features1[j] - trainingDataMat.at<float>(i, j)) / 1000.0);
+				tmp += (features[j] - trainingDataMat.at<float>(i, j)) * (features[j] - trainingDataMat.at<float>(i, j));
 			}
 			tmp = sqrt(tmp);
 
@@ -93,10 +81,15 @@ int _tmain(int argc, _TCHAR* argv[])
 				k = i;
 			}
 		}
-		cout << k << " " << labelsMat.at<float>(0, k) << " " << endl;
-	}
 
-	waitKey(0);
+		if (labelsMat.at<float>(0, k) == ((i - 1) / 5 + 1)) {
+			right += 1;
+		}
+
+		std::cout << i << ".png - " << (i - 1) / 5 + 1 << " " << labelsMat.at<float>(0, k) << std::endl;
+	}
+	
+	cout << (right/56.0) * 100 << "%" << endl;
 	system("pause");
 	return 0;
 }
